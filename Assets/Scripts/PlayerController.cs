12f37d052e1adc40357fe1d2a67b4e1e397e0c65
mogan42 <mo.g@live.com.au>
2017-07-14 20:36:30 +0930

    made char controller and have added a road texture

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    CharacterController cC;
+
+    Vector3 moveDirection;
+    Vector3 LookDirection;
+
+    public float playerSpeed;
+    public float turnSpeed;
+
+
+    bool isMoving;
+    bool isLooking;
+
+    //Quaternion lookDirection;
+    //Quaternion curentLookDirection;
+
+
+	// Use this for initialization
+	void Start () {
+        cC = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        PlayerMoveDirection();
+        //curentLookDirection = Quaternion.LookRotation(moveDirection);
+        //lookDirection = Quaternion.RotateTowards(curentLookDirection, lookDirection, Time.deltaTime * turnSpeed);
+        
+        cC.SimpleMove(moveDirection * playerSpeed);
+
+	}
+
+    void PlayerMoveDirection()
+    {
+        moveDirection = new Vector3(Input.GetAxisRaw("Vertical"), 0f,-Input.GetAxisRaw("Horizontal"));
+        
+    }
+
+}
