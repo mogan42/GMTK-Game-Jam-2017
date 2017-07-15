02f7287c6651eb588189238248299e0195a9ce19
mogan42 <mo.g@live.com.au>
2017-07-15 16:34:38 +0930

    adding sound to the game also enemys die now.

---
+++ Assets/Scripts/DamageEnemys.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageEnemys : MonoBehaviour {
+    
+    public float healthToTake;
+    private Enemy cHealth;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            cHealth = other.GetComponent<Enemy>();
+            cHealth.health = cHealth.health - healthToTake;
+            Destroy(gameObject);
+            
+        }
+    }
+}
